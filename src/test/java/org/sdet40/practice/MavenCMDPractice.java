9e118bdbb9e7a327edc22a5c94176776b2149b04
sncsr <sncsr@LAPTOP-H7R7DK85>
2022-09-21 12:49:50 +0530

    changes done in workspace 1

--- src/test/java/org/sdet40/practice/MavenCMDPractice.java
+++ src/test/java/org/sdet40/practice/MavenCMDPractice.java
@@ -10,7 +10,7 @@ public class MavenCMDPractice {
 	public void test1() {
 		String browser = System.getProperty("browser");
 		String url = System.getProperty("url");
-	
+	//Open brower
 		WebDriverUtility webdriver = new WebDriverUtility();
 		WebDriver driver = webdriver.launchBrowser(browser);
 		webdriver.maximizeBrowser();
