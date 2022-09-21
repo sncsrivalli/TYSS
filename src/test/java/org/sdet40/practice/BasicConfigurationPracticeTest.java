9ee37a18ac295fae5b31dbecd5cfb9d6e30c5eaa
sncsrivalli <109685225+sncsrivalli@users.noreply.github.com>
2022-09-21 13:10:31 +0530

    Update BasicConfigurationPracticeTest.java

--- src/test/java/org/sdet40/practice/BasicConfigurationPracticeTest.java
+++ src/test/java/org/sdet40/practice/BasicConfigurationPracticeTest.java
@@ -11,7 +11,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class BasicConfigurationPracticeTest {
-	
+	//TestNG practice
 	@BeforeSuite
 	public void suite1Setup() {
 		System.out.println("suite1");
